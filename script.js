602bf11542274d0d6c9bdea610d24de843a10da9
Ilangasinghe I M H H <102861747+enghirushan@users.noreply.github.com>
2024-02-22 17:28:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//string Data type
+let username = "Haritha";
+console.log(username, typeof username);
+
+//number data type
+let age = 25;
+console.log(age, typeof age);
+
+//boolean data type
+let Male=true;
+console.log(Male, typeof Male);
+
+//undefined data type
+let undef= undefined;
+console.log(undef, typeof undef);
+
+//null data type
+let nul=null;
+console.log(nul, typeof nul);  
+
+//object data type
+const student = {
+  name: "Haritha",
+  age: 25,
+  gender: "Male"
+};
+console.log(student.name ,  typeof student);
+console.log(student.age , typeof student);
+console.log(student.gender , typeof student);
